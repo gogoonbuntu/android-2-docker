d04eaaccda81512e32b87600e78505f81886c769
Gogoonbuntu <34636395+gogoonbuntu@users.noreply.github.com>
2025-01-04 00:04:14 +0900

    Create 4-qemu-start-alpine.sh

---
+++ 4-qemu-start-alpine.sh
@@ -0,0 +1,6 @@
+qemu-system-x86_64 -machine q35 -m 2048 -smp cpus=6 -cpu qemu64 \
+    -drive if=pflash,format=raw,read-only=on,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
+    -netdev user,id=n1,hostfwd=tcp::2222-:22 \
+    -device virtio-net,netdev=n1 \
+    -cdrom alpine-virt-3.15.3-x86_64.iso \
+    -nographic alpine.img
